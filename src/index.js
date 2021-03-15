c7dd54c0913ebfa7b24020e5b1156148412e0f6d
tandav21 <tvdavyd@gmail.com>
2021-03-15 18:56:19 +0300

    feat: add new js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    //let num = toString(n);
+    //let arr = num.split('');
+    //let rev = arr.reverse(); 
+    //let strN = rev.join('');
+    //return strN;
+    return parseInt(n.toString().split('').reverse().join(''));
 }
